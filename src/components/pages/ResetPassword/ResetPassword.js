e2587d1edf616578105ad161839bae49ec41cd42
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-01-09 14:36:33 +0100

    #9 install formik and refactor reset password page using it

--- src/components/pages/ResetPassword/ResetPassword.js
+++ src/components/pages/ResetPassword/ResetPassword.js
@@ -2,43 +2,32 @@ import React, { PureComponent } from 'react';
 import { View, Text, TextInput } from 'react-native';
 import Button from '../../atoms/Button';
 import { isInputRequired } from '../../../helpers/validation';
+import { EMAIL } from '../../../helpers/fieldNames';
 import style from '../../../assets/style';
 
 class ResetPassword extends PureComponent {
-  state = {
-    email: '',
-    error: null,
-    submitting: false
-  }
-
-  handleSubmit = () => {
-    const { email, submitting } = this.state;
-    this.setState({
-      emailRequired: !email,
-    });
-
-    if (email && !submitting) {
-      this.setState({ submitting: true });
-      // TODO: fetch to create the account
-      setTimeout(() => this.setState({ submitting: false }), 500);
-    }
-  }
-
   render() {
-    const { goBack } = this.props.navigation;
-    const { error, emailRequired, submitting } = this.state;
+    const {
+      errors,
+      handleSubmit,
+      handleChange,
+      handleBlur,
+      isSubmitting,
+      navigation: { goBack }
+    } = this.props;
 
     return (
       <View style={[style.container, style.content]}>
         <Text style={style.label}>Email</Text>
         <TextInput
-          style={isInputRequired(emailRequired)}
-          onChangeText={email => this.setState({ email })}
+          style={isInputRequired(errors.email)}
+          onChangeText={handleChange(EMAIL)}
+          onBlur={handleBlur(EMAIL)}
         />
         <Button
-          onPress={this.handleSubmit}
+          onPress={handleSubmit}
           text='Request reset password'
-          loading={submitting}
+          loading={isSubmitting}
         />
         <Button
           onPress={() => goBack()}
--- src/components/pages/ResetPassword/index.js
+++ src/components/pages/ResetPassword/index.js
@@ -1,3 +1,25 @@
 import ResetPassword from './ResetPassword';
+import { withFormik } from 'formik';
 
-export default ResetPassword;
+const MyEnhancedForm = withFormik({
+  mapPropsToValues: () => ({ email: '' }),
+
+  validate: ({ email }) => {
+    const errors = {};
+    if (!email) {
+      errors.email = true;
+    }
+    return errors;
+  },
+
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  },
+
+  displayName: 'BasicForm',
+})(ResetPassword);
+
+export default MyEnhancedForm;
