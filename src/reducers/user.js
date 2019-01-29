0e0d3f3aa72b7673dbcab894784167e5a75a5973
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-01-30 00:11:39 +0100

    Create initial profile page and add initial redux

---
+++ src/reducers/user.js
@@ -0,0 +1,14 @@
+const initialState = {
+  name: '',
+  email: ''
+};
+
+const user = (state = initialState, { type }) => {
+  switch (type) {
+    default: {
+      return state
+    }
+  }
+};
+
+export default user
